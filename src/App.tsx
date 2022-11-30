cd5b87275889b66577d315b3e5bfe1b3e7988850
Oleksii Sheiko <oleksii.sheiko@otakoyi.com>
2022-12-01 01:22:41 +0200

    Replaced BrowserRouter with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, Quiz, Result } from 'components/pages';
 
 function App() {
